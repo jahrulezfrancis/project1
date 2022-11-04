256f814db7c258011594de925bf5e21bc5fb42fa
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-04 23:15:01 +0100

    updated header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,20 @@
 import Logo from "./Images/react-logo-vector.svg"
+import "./body.css"
 
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <div className="Header">
-              <header>
-                <nav>
-                    <img src={Logo} alt='Header Logo' width={'80px'} height={'80px'} />
+            <header>
+                <nav className="navigationItems">
+                    <ul>
+                        <li className="header-logo">
+                            <img src={Logo} alt='Header Logo' width={'80px'} height={'80px'} />
+                        </li>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li className="end-item">Contact</li>
+                    </ul>
                 </nav>
             </header>
         </div>

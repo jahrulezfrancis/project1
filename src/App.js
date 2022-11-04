69936190412ba3e0b78d27ab91e2a272d7e84c6d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-04 23:12:05 +0100

    added body and footer compnent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Body } from './Body';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Body />
+      <Footer />
     </div>
   );
 }

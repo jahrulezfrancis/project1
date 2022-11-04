6f381ac7b791bc1f7844f02849268363eca4d5a6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-04 10:37:31 +0100

    initial commit

---
+++ src/Body.jsx
@@ -0,0 +1,9 @@
+export function Body(){
+    return(
+        <div className="body">
+            <div className="bodyHeader">
+                <h1>Fun fact about learning React</h1>
+            </div>
+        </div>
+    )
+}

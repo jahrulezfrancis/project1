f2b232cfad966acdf7951458aae035a64db267a3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-04 23:12:51 +0100

    created footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div>
+            <footer className="footer-section">
+                <small>
+                    © Copyright 2022 7thWeb Technologies
+                </small>
+            </footer>
+        </div>
+    )
+}

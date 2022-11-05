067bc2dbd5fb0df13e8e0c033a62b1a4f25333f3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-05 20:05:56 +0100

    added Id to company list

--- src/DataBase/companies.js
+++ src/DataBase/companies.js
@@ -1,17 +1,22 @@
 export const Companies = [
     {
+        id: 1,
         name: 'Facebook'
     },
     {
+        id: 2,
         name: 'Twitter'
     },
     {
+        id: 3,
         name: 'Netflix'
     },
     {
+        id: 4,
         name: 'Air BNB'
     },
     {
+        id: 5,
         name: 'Udemy'
     },
 ]

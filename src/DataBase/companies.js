eecf5d766a7508358b6ad9f53e3bd5341f232ca3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-05 20:01:09 +0100

    created company object

---
+++ src/DataBase/companies.js
@@ -0,0 +1,17 @@
+export const Companies = [
+    {
+        name: 'Facebook'
+    },
+    {
+        name: 'Twitter'
+    },
+    {
+        name: 'Netflix'
+    },
+    {
+        name: 'Air BNB'
+    },
+    {
+        name: 'Udemy'
+    },
+]

ec40b064b22f173aa330f8178779f4b834caf16d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-05 20:08:29 +0100

    updated company list component

--- src/CompanyList.jsx
+++ src/CompanyList.jsx
@@ -1,7 +1,7 @@
 export function CompanyList() {
     return (
         <div className="companyList">
-
+            <h4>React is widely used by a lot of tech companies now, because of how scalable it is</h4>
         </div>
     )
 }
